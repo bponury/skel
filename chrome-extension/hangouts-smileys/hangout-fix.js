7a65ad2e986529a52ec0a3ab543e8e08d807034b
Bartosz Ponurkiewicz <bartosz@ponury.net>
2015-04-19 11:41:59 +0100

    Chrome extension for disabling hangout smileys

---
+++ chrome-extension/hangouts-smileys/hangout-fix.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	var shift = false;
+	$(document).keydown(function(ev) {
+		if (ev.which == 16)
+			shift = true;
+		if (ev.which == 13 && !shift) {
+			var map = {
+				'600': ':D',
+				'603': ':)',
+				'609': ';)',
+				'61c': ';P',
+				'61b': ':P',
+			};
+
+			var str = $('div.PM .editable').html();
+			for (var key in map) {
+				str = str.replace(new RegExp('<img src="//ssl.gstatic.com/chat/emoji/4/emoji_u1f' + key + '.png" [^>]+>', 'g'), map[key]);
+			}
+			$('div.PM .editable').html(str);
+		}
+	});
+	$(document).keyup(function(ev) {
+		if (ev.which == 16)
+			shift = false;
+	});
+});
