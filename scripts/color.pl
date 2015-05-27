3719c2c123ba2be0e4a77b8a457dfb09d1415c28
Bartosz [ponury] Ponurkiewicz <ponury@google.com>
2015-05-27 16:32:33 +0100

    Use nonblocking IO for color.pl script to work nicely with non-newline ending input

--- scripts/color.pl
+++ scripts/color.pl
@@ -1,24 +1,35 @@
 #!/usr/bin/perl
 # Bartosz [ponury] Ponurkiewicz
 
-use strict;
+#use strict;
+use warnings;
+use IO::Select;
 
 $#ARGV >= 0 or die "$0 <regex#1> [regex#2] ...";
 
 my @pattern = @ARGV or die $!;
 my $line;
 
-while ($line = <STDIN>) {
-	my $i = 1;
+my $sel = IO::Select->new();
+$sel->add(\*STDIN);
 
-	foreach (@pattern) {
-		my $c = "\033\[01;3" . $i++ . "m";
-		if ($i % 8 == 0) {
-			$i = 1;
+$| = 1;
+while (@ready = $sel->can_read) {
+	foreach $fh (@ready) {
+		my $line;
+		if (sysread($fh, $line, 10240) < 1) {
+			exit 0;
 		}
+		my $i = 1;
 
-		$line =~ s/($_)/$c\1\033\[0;0m/gi;
+		foreach (@pattern) {
+			my $c = "\033\[01;3" . $i++ . "m";
+			if ($i % 8 == 0) {
+				$i = 1;
+			}
+
+			$line =~ s/($_)/$c$1\033\[0;0m/gi;
+		}
+		print $line;
 	}
-
-	print $line;
 }
